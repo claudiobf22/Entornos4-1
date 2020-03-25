6a8dcfdcbad7c2488ae88c30d8f382ccb0990b17
claudiobf22 <57524471+claudiobf22@users.noreply.github.com>
2020-03-25 11:37:32 +0100

    Preparación del ejercicio

---
+++ src/Ejercicio.java
@@ -0,0 +1,30 @@
+public class Ejercicio {
+        private double numero;
+
+        //getters y setters
+        public double getNumero() {
+            return numero;
+        }
+
+        public void setNumero(double numero) {
+            this.numero = numero;
+        }
+
+        public double calcularRaizCuadrada() {
+            return Math.sqrt(numero);
+        }
+
+        public double calcularRaizCubica() {
+            return Math.abs(Math.cbrt(numero));
+        }
+        public int redondearAEntero() {
+            return (int) Math.round(numero);
+        }
+
+        public int obtenerNumeroAleatorio() {
+            return (int) Math.round( (redondearAEntero() * Math.random()));
+        }
+
+    }
+
+
